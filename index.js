ac2d128bc5cbac8e5161502079b56416eea9de97
Lawson Hung <lhung1@babson.edu>
2019-04-20 02:43:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
+  var i;
   
-  for (let int i = 0; i < musicians.length; i++) {
-    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  for (let i = 0; i < musicians.length; i++) {
+    array[i].push(`${musicians[i]} plays ${instruments[i]}`);
   }
 }
